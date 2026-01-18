a0dff4cb5400c60c51734d0bce0bb86698fb4e03
atharvatamta <tamta.atharva@gmail.com>
2026-01-18 13:45:46 +0530

    using local storage for saving notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import NoteForm  from "./components/NoteForm";
-import { useState } from "react";
+import NoteForm from "./components/NoteForm";
+import { useState, useEffect } from "react";
 import NoteList from "./components/NoteList";
 
 const App = () => {
-const [notes,setNotes] = useState([])
-
-  return (  <div className =" max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg"
-  >
-    <h2 className="text-2xl font-bold mb-4 text-center">📝 Notes App</h2>
-    <NoteForm notes= {notes} setNotes= {setNotes}/>
-    <NoteList notes= {notes}/>
-  </div>);
-}
+  const [notes, setNotes] = useState(() => {
+    const notes = JSON.parse(localStorage.getItem("notes"));
+    return notes || [];
+  });
+  useEffect(() => {
+    localStorage.setItem('notes',JSON.stringify(notes))
+  }, [notes]);
+  return (
+    <div className=" max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg">
+      <h2 className="text-2xl font-bold mb-4 text-center">📝 Notes App</h2>
+      <NoteForm notes={notes} setNotes={setNotes} />
+      <NoteList notes={notes} />
+    </div>
+  );
+};
 export default App;
 
 // add a delete button and create a function which will be passed up to the app.jsx containing global state and then filtering out the note using notee id
+// make a component so that Notelist looks cleaner
